734bea99485da120088960c618dd95f63ab82196
nehaa4 <nehaa4@illinois.edu>
2021-11-05 22:49:11 -0500

    Added empty flutter project with extra files and navigation comments

---
+++ flutter/wece_glasses/android/app/src/main/kotlin/com/example/wece_glasses/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wece_glasses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/wece_glasses/lib/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/*
+ Here is where we will constants relating to the theme of our app, such as
+ color scheme, font settings, and other UI objects as necessary
+ */
+
+class AppColors {
+  /*
+   Here are WECE's theme colors for 2021-2022 courtesy of marketing.
+   We don't necessarily have to use these colors, but if we do they should be
+   renamed appropriately. Leaving them here for reference.
+  */
+  // Flutter colors are RGB color codes prefixed with an opacity value.
+  // The FF represents full opacity
+  static const color1 = Color(0xFF2A5236);
+  static const color2 = Color(0xFFF7E7CE);
+  static const color3 = Color(0xFFE6ABB5);
+}
+
+// ignore: non_constant_identifier_names
+final AppTheme = ThemeData(
+  // TODO Add AppTheme
+  // See Flutter theme documentation for details
+  // https://flutter.dev/docs/cookbook/design/themes
+);
