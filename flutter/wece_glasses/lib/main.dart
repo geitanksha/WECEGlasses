a63723e6df0814cc1ff0d43ccf4f118eeabce11b
Parvati03 <menonp2021@gmail.com>
2022-01-22 12:19:56 -0600

    adding weather and main files

--- flutter/wece_glasses/lib/main.dart
+++ flutter/wece_glasses/lib/main.dart
@@ -2,101 +2,91 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'theme.dart';
 import 'constants.dart';
+import 'weatherAPI.dart';
+
+
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // The app theme is defined in constants.dart
-      title: Constants.appName,
-      // The app theme is defined in theme.dart
-      theme: AppTheme,
-      // This directs the app to start at the screen class called MyHomePage.
-      // Currently the main screen is the default from Flutter, but we will
-      // edit the screens and screen routes as needed.
-      home: const MyHomePage(title: Constants.appName),
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
     );
   }
 }
 
-// This is the default home page defined by Flutter. In the future,
-// this will be moved out of main.dart for better organization.
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String _connectedDevice = ""; // Default no device selected
-
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  Color buttonColor = Colors.grey;
+  bool buttonOn = false;
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      // Set of widgets will be arranged vertically
-      body: Column(
+    //final ButtonStyle style =
+    //ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          // Widgets will be arranged horizontally
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Connected Device: ',
-              ),
-              DropdownButton<String>(
-                value: _connectedDevice,
-                icon: const Icon(Icons.beach_access),
-                iconSize: 15,
-                elevation: 16,
-                underline: Container(
-                  height: 2,
-                  color: Colors.deepPurpleAccent, // TODO change to theme
-                ),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _connectedDevice = newValue!;
-                  });
+          ElevatedButton(
+            child: Text('Elevated Button'),
+            style: ElevatedButton.styleFrom(
+                primary: (buttonOn ? Colors.grey : Colors.purple),
+                elevation: 8,
+                shape: CircleBorder(),
+                minimumSize: Size.square(80)
+            ),
+            onPressed: () {
+              setState ((){
+                if (buttonOn == true) {
+                  buttonOn  = false;
+                }
+                else{
+                  buttonOn  = true;
+                }
+              });
+              print('Pressed');
+            },
+
+            /*style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith<Color>(
+
+                    (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed)) return Colors.blue;
+                  return Colors.grey;
                 },
-                // This is an example list of items.
-                // We will need to generate this list programmatically
-                items: <String>['', 'Device 1', 'Device 2', 'Device 3', 'Device 4']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
               ),
-            ],
+            ),*/
           ),
-          // TODO add screen management buttons
+
+          IconButton(
+            icon: const Icon(Icons.cloud),
+            //tooltip: 'Increase volume by 10',
+            onPressed: () {
+              setState(() {
+                fetchWeather();
+              });
+            },
+          ),
         ],
       ),
-    );
+      );
   }
 }
--- flutter/wece_glasses/lib/weatherAPI.dart
+++ flutter/wece_glasses/lib/weatherAPI.dart
@@ -1,24 +1,22 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-Future<http.Response> fetchWeather() {
-  return http.get(Uri.parse('https://api.weather.gov/gridpoints/TOP/40,88/forecast'));
-}
 //need temperature
 //need detailedForecast
+
 class Weather {
-  final String temperature;
+  final int temperature;
   final String detailedForecast;
 
   Weather(this.temperature, this.detailedForecast);
 
-  Weather.fromJson(Map<String, dynamic> json)
-      : temperature = json['temperature'],
-        detailedForecast = json['detailedForecast'];
+   Weather.fromJson(Map<String, dynamic> json)
+      : temperature = json["properties"]["periods"][0]['temperature'],
+        detailedForecast =  json["properties"]["periods"][0]['detailedForecast'];
 
-  Map<String, dynamic> toJson() => {
-    'temperature': temperature,
-    'detailedForecast': detailedForecast,
-  };
-}
+
+} // class Weather
+
 Future<Weather> fetchWeather() async {
   final response = await http
       .get(Uri.parse('https://api.weather.gov/gridpoints/TOP/40,88/forecast'));
@@ -26,34 +24,13 @@ Future<Weather> fetchWeather() async {
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return Weather.fromJson(jsonDecode(response.body));
+    print("cloud clicked");
+    Weather weather =  Weather.fromJson(jsonDecode(response.body));
+    print(weather.temperature);
+    return weather;
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
-class _MyAppState extends State<MyApp> {
-  late Future<Album> futureAlbum;
-
-  @override
-  void initState() {
-    super.initState();
-    futureWeather = fetchAWeather();
-  }
-// ···
-}
-
-FutureBuilder<Weather>(
-  future: futureWeather,
-  builder: (context, snapshot) {
-      if (snapshot.hasData) {
-        return Text(snapshot.data!.title);
-      }   else if (snapshot.hasError) {
-      return Text('${snapshot.error}');
-    }
-
-  // By default, show a loading spinner.
-      return const CircularProgressIndicator();
-    },
-)
