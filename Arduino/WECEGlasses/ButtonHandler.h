48a64cfce4f4902d801065acf0bd20fd9b55866e
raina kapadia <rainakapadia@wirelessprv-10-195-4-148.near.illinois.edu>
2022-01-29 14:02:41 -0600

    button long and short press

--- Arduino/WECEGlasses/ButtonHandler.h
+++ Arduino/WECEGlasses/ButtonHandler.h
@@ -9,6 +9,11 @@ class ButtonHandler {
   private:
     int lastState = LOW; // Previous state of the button
     int currentState; // Current state of the button
+    unsigned long pressedTime=0;
+    unsigned long releasedTime=0;
+    const int SHORT_PRESS_TIME=1000;
+    const int LONG_PRESS_TIME=1000;
+     
     
   public:
     // Simple logic for reading a button click
@@ -18,9 +23,18 @@ class ButtonHandler {
       
       currentState = digitalRead(BUTTON_PIN);
 
-      if(lastState == HIGH && currentState == LOW) {
+      if(lastState == HIGH && currentState == LOW) 
         // Button was released (That is, pressed then released)
-        ret = 1;
+        pressedTime= millis();
+      else if(lastState==LOW && currentState== HIGH){ 
+        releasedTime=millis();
+
+         long pressedDuration=releasedTime-pressedTime;
+         if (pressDuration<SHORT_PRESS_TIME)
+          ret=1; //short press detected
+
+        if( pressDuration > LONG_PRESS_TIME )
+          ret=2;  //long press detected
       }
 
       lastState = currentState;
