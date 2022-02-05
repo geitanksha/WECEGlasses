82faa5ee6377c1b8a765c8e5c23681ab8ebca5a6
Parvati03 <menonp2021@gmail.com>
2022-02-05 14:58:55 -0600

    adding location files, and updating weatherAPI

--- flutter/wece_glasses/lib/weatherAPI.dart
+++ flutter/wece_glasses/lib/weatherAPI.dart
@@ -3,66 +3,13 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:location/location.dart';
+import 'UseLocation.dart';
+import 'LocationAccess.dart';
+
 //need temperature
 //need detailedForecast
 
 class Weather {
-  var location = new Location();
-  PermissionStatus permissionGranted;
-  LocationData locationData;
- void locationAccess() async {
-
-
-
-    bool serviceEnabled = await location.serviceEnabled();
-
-    if(!serviceEnabled){
-      serviceEnabled = await location.requestService();
-
-      if(!serviceEnabled){
-        return;
-      }
-    }
-    permissionGranted = await location.hasPermission();
-
-    if(permissionGranted == PermissionStatus.denied){
-        permissionGranted = await location.requestPermission();
-      if(permissionGranted != PermissionStatus.granted){
-        return;
-      }
-    }
-  }
-
-  Future<String> fetchLocation() async{
-
-    var currentLocation = await location.getLocation();
-
-    var latitude = locationData.latitude;
-    var longitude = locationData.longitude;
-
-    var forecast;
-    var gridX;
-    var gridY;
-
-    LocationURL.fromJson(Map<String, dynamic> json)
-        : forecast = json["properties"]["forecast"],
-          gridX = json["properties"]["gridX"],
-          gridY = json["properties"]["gridY"];
-
-    final response = await http
-        .get(Uri.parse('https://api.weather.gov/points/'+ latitude +',' + longitude));
-    if (response.statusCode == 200) {
-        // If the server did return a 200 OK response,
-        // then parse the JSON.
-
-        return forecast;
-
-    } else {
-        // If the server did not return a 200 OK response,
-        // then throw an exception.
-        throw Exception('Failed to load album');
-    }
-  }
 
   final int temperature;
   final String detailedForecast;
@@ -75,10 +22,10 @@ class Weather {
 
 } // class Weather
 
-Future<Weather> fetchWeather(String url) async {
-  fetchLocation();
+Future<Weather> fetchWeather() async {
+
   final response = await http
-      .get(Uri.parse(url));
+      .get(Uri.parse(fetchLocation().toString()));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
