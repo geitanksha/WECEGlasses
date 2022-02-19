73ae05d07dad9be5101549ae135a233484ee91b7
Cathy Boman <cathyboman@wirelessprv-10-192-80-29.near.illinois.edu>
2022-02-19 15:21:42 -0600

    started working on start-up screen

--- Arduino/WECEGlasses/DisplayHandler.h
+++ Arduino/WECEGlasses/DisplayHandler.h
@@ -16,6 +16,7 @@
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define OLED_RESET     4 // Reset pin 
 #define SCREEN_ADDRESS 0x3C 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 class DisplayHandler {
   public:
@@ -23,7 +24,40 @@ class DisplayHandler {
     Adafruit_SSD1306 oled = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
     // TODO Add methods for writing information to screen
+    void setup(){
+      Serial.begin(9600);
+  
+      // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+      if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Don't proceed, loop forever
+      }
     
+      // Show initial display buffer contents on the screen --
+      // the library initializes this with an Adafruit splash screen.
+      display.display();
+      delay(2000); // Pause for 2 seconds
+    
+      // Clear the buffer
+      display.clearDisplay();
+    
+      // Draw a single pixel in white
+      display.drawPixel(10, 10, SSD1306_WHITE);
+    
+      // Show the display buffer on the screen. You MUST call display() after
+      // drawing commands to make them visible on screen!
+      display.display();
+      delay(2000);
+  
+      testscrolltext();    // Draw scrolling text
+        
+      display.invertDisplay(true);
+      delay(1000);
+      display.invertDisplay(false);
+      delay(1000);
+    }
+
+    /*
     void writeSimpleString(String text) {
       // Very simple example function for writing small text to screen
       oled.clearDisplay();
@@ -36,8 +70,11 @@ class DisplayHandler {
 
       // Pixels are only drawn to screen once display method is called
       oled.display();
+
+      
     }
-    
+    */
+     
     // Constructor
     void init() {
       Serial.println("Initializing display");
@@ -49,11 +86,23 @@ class DisplayHandler {
       }
       
       // TODO Add startup screen
-      // Here's a basic placeholder
-      writeSimpleString("WECE Glasses");
+      //#include
+      /*
+      const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+      LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+      void setup() {
+        lcd.begin(16, 2);
+        lcd.print("hello, world!");
+      }
+      void loop() {
+        lcd.setCursor(0, 1);
+        lcd.print(millis() / 1000);
+      }  
+      */
+
+      //writeSimpleString("WECE Glasses");
     }
 
-    
   private:
   
 };
