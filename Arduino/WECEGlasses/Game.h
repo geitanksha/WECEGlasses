cb0d1a298bd936629f0a111c199b850c909e00b1
Kaitlin Gowens <gokaitgo100@gmail.com>
2022-03-02 17:47:02 -0600

    Moving rectangles

--- Arduino/WECEGlasses/Game.h
+++ Arduino/WECEGlasses/Game.h
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include ButtonHandler.h
+#include ButtonHandler.h // Use this for button?? 
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
@@ -33,20 +33,65 @@ void setup() {
     display.setTextSize(2);             // Draw 2X-scale text
     display.setTextColor(SSD1306_WHITE);
     display.print(F("Shooting Star!")); display.println(F("Press button to start!"));
-    button_state= digitalRead(BUTTON_PIN);
-    if (ButtonHandhler.h.readState() == 1){
+    button_state= digitalRead(BUTTON_PIN);      // CHANGE ONCE BUTTON FUNCTION DECIDED
+    if (button_state == 1){
       playing = true;
     }
     
  }
-void testfillrect(void) {
+void moverect(void) {
   display.clearDisplay();
 
   for(int16_t i=0; i<display.height()/2; i+=3) {
-    // The INVERSE color is used so rectangles alternate white/black
-    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, SSD1306_INVERSE);
+    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, SSD1306_BLACK);
     display.display(); // Update screen with each newly-drawn rectangle
     delay(1);
   }
-testfillrect();
-  delay(2000);
+moverect();
+  delay(200);
+
+#define XPOS   0 // Indexes into the 'obstacles' array in function below
+#define YPOS   1
+#define DELTAX 2
+#define NUMRECT 10 // FIND WAY TO MAKE THESE GOING UNTIL HIT
+
+void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  int8_t f, obstacles[NUMRECT][3];
+
+  // Initialize 'snowflake' positions
+  for(f=0; f< NUMRECT; f++) {
+    obstacles[f][XPOS]   = display.width();
+    obstacles[f][YPOS]   = display.height() - 5;
+    obstacles[f][DELTAX] = random(1, 6);
+    Serial.print(F("x: "));
+    Serial.print(obstacles[f][XPOS], DEC);
+    Serial.print(F(" y: "));
+    Serial.print(obstacles[f][YPOS], DEC);
+    Serial.print(F(" dy: "));
+    Serial.println(obstacles[f][DELTAY], DEC);
+  }
+
+  for(;;) { // Loop forever...
+    display.clearDisplay(); // Clear the display buffer
+
+    // Draw each snowflake:
+    for(f=0; f< NUMRECT; f++) {
+      display.fillRect(obstacles[f][XPOS], obstacles[f][YPOS], w, h, SSD1306_BLACK);
+    }
+
+    display.display(); // Show the display buffer on the screen
+    delay(200);        // Pause for 1/10 second
+
+    // Then update coordinates of each flake...
+    for(f=0; f< NUMRECT; f++) {
+      obstacles[f][XPOS] -= obstacles[f][DELTAX];
+      // If snowflake is off the bottom of the screen...
+      if (obstacles[f][XPOS] <= display.width()) {
+        // Reinitialize to a random position, just off the top
+        obstacles[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+        obstacles[f][YPOS]   = -LOGO_HEIGHT;
+        obstacles[f][DELTAX] = random(1, 6);
+      }
+    }
+  }
+}
