2039c50529db3ff9745c71fdf4ff7e8c2ff2e336
Kaitlin Gowens <gokaitgo100@gmail.com>
2022-03-02 16:01:28 -0600

    redesign app layout

--- flutter/wece_glasses/lib/homepage.dart
+++ flutter/wece_glasses/lib/homepage.dart
@@ -60,83 +60,88 @@ class _HomePageState extends State<HomePage> {
           centerTitle: true,
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(bleHandler.connectedDevice == null
-                  ? "Please connect a device"
-                  : bleHandler.connectedDevice!.name),
-              ElevatedButton(
-                onPressed: bleHandler.connectedDevice == null
-                    ? connectDevicePrompt
-                    : disconnectDevice,
-                child: Text(bleHandler.connectedDevice == null
-                    ? "Connect"
-                    : "Disconnect"),
-              ),
-              // TODO Add rest of user functionality (settings, etc.) here
-              if (bleHandler.connectedDevice != null)
-                // Temporary to test screen changes
-                ElevatedButton(
-                    onPressed: () => deviceScreenHandler.nextScreen(),
-                    child: const Text("Next Screen")),
-              if (bleHandler.connectedDevice != null)
-                Ink(
-                  width:200,
-                  height: 60,
-                  child: GridView.count(
-                    primary: true,
-                    crossAxisCount:3,
-                    //set the number of buttons in a row
-                    crossAxisSpacing: 8,
-                    //set the spacing between the buttons
-                    childAspectRatio: 1,
-                    //set the width-to-height ratio of the button,
-                    //>1 is a horizontal rectangle
-                    children: List.generate(
-                        deviceScreenHandler.displayScreenOn.length, (index) {
-                      //using Inkwell widget to create a button
-                      return InkWell(
-                          splashColor: AppColors.pink,
-                          //the default splashColor is grey
-                          onTap: () {
-                            //set the toggle logic
-                            int count = 0;
-                            for (var val in deviceScreenHandler.displayScreenOn) {
-                              if (val) count++;
-                            }
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'lib/Image/Top_Banner.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                    Text(bleHandler.connectedDevice == null
+                        ? "Please connect a device"
+                        : bleHandler.connectedDevice!.name),
 
-                            // At least one screen should be selected at all times
-                            if (deviceScreenHandler.displayScreenOn[index] && count < 2) {
-                              return;
-                            }
+                    ElevatedButton(
+                      onPressed: bleHandler.connectedDevice == null
+                          ? connectDevicePrompt
+                          : disconnectDevice,
+                      child: Text(bleHandler.connectedDevice == null
+                          ? "Connect"
+                          : "Disconnect"),
+                    ),
+                    // TODO Add rest of user functionality (settings, etc.) here
+                    if (bleHandler.connectedDevice != null)
+                      // Temporary to test screen changes
+                      ElevatedButton(
+                          onPressed: () => deviceScreenHandler.nextScreen(),
+                          child: const Text("Next Screen")),
+                    if (bleHandler.connectedDevice != null)
+                      Ink(
+                        width:200,
+                        height: 60,
+                        child: GridView.count(
+                          primary: true,
+                          crossAxisCount:3,
+                          //set the number of buttons in a row
+                          crossAxisSpacing: 8,
+                          //set the spacing between the buttons
+                          childAspectRatio: 1,
+                          //set the width-to-height ratio of the button,
+                          //>1 is a horizontal rectangle
+                          children: List.generate(
+                              deviceScreenHandler.displayScreenOn.length, (index) {
+                            //using Inkwell widget to create a button
+                            return InkWell(
+                                splashColor: AppColors.pink,
+                                //the default splashColor is grey
+                                onTap: () {
+                                  //set the toggle logic
+                                  int count = 0;
+                                  for (var val in deviceScreenHandler.displayScreenOn) {
+                                    if (val) count++;
+                                  }
 
-                            // Note that if the current screen is deselected,
-                            // it will only take effect once user moves to next screen
-                            setState(() {
-                              deviceScreenHandler.displayScreenOn[index] =
-                              !deviceScreenHandler.displayScreenOn[index];
-                            }); //setState
-                          },
-                          child: Ink(
-                            decoration: BoxDecoration(
-                              //set the background color of the button when it is selected/ not selected
-                              color: deviceScreenHandler.displayScreenOn[index] ? AppColors.lightBlue : Colors.white,
-                              // here is where we set the rounded corner
-                              borderRadius: BorderRadius.circular(8),
-                              //don't forget to set the border,
-                              //otherwise there will be no rounded corner
-                              border: Border.all(color: AppColors.darkBlue),
-                            ),
-                            child: Icon(deviceScreenHandler.screens[index].getIcon(),
-                                //set the color of the icon when it is selected/ not selected
-                                color: deviceScreenHandler.displayScreenOn[index] ? AppColors.pink : Colors.grey),
-                          ));
-                    }),
-                  ),
+                                  // At least one screen should be selected at all times
+                                  if (deviceScreenHandler.displayScreenOn[index] && count < 2) {
+                                    return;
+                                  }
+
+                                  // Note that if the current screen is deselected,
+                                  // it will only take effect once user moves to next screen
+                                  setState(() {
+                                    deviceScreenHandler.displayScreenOn[index] =
+                                    !deviceScreenHandler.displayScreenOn[index];
+                                  }); //setState
+                                },
+                                child: Ink(
+                                  decoration: BoxDecoration(
+                                    //set the background color of the button when it is selected/ not selected
+                                    color: deviceScreenHandler.displayScreenOn[index] ? AppColors.lightBlue : Colors.white,
+                                    // here is where we set the rounded corner
+                                    borderRadius: BorderRadius.circular(8),
+                                    //don't forget to set the border,
+                                    //otherwise there will be no rounded corner
+                                    border: Border.all(color: AppColors.darkBlue),
+                                  ),
+                                  child: Icon(deviceScreenHandler.screens[index].getIcon(),
+                                      //set the color of the icon when it is selected/ not selected
+                                      color: deviceScreenHandler.displayScreenOn[index] ? AppColors.pink : Colors.grey),
+                                ));
+                          }),
+                        ),
+                      ),
+                  ],
                 ),
-            ],
-          ),
         )
       ),
     );
